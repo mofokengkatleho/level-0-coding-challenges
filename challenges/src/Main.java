4ccc5fa522f9e571c41add4cae3ef9bf6717d5e1
katleho <katlehomofokeng@protonmail.com>
2021-11-13 10:04:40 +0200

    first commit

---
+++ challenges/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void task1(){
+        int x = 0;
+        int y = 1;
+        System.out.println(x);
+        System.out.println(y);
+        x += 3;
+        y += x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        task1();
+    }
+}
