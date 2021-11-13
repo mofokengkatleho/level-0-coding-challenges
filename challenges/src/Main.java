672adaf57603c7aa7f13caba8705d01d169bc1b6
katleho <katlehomofokeng@protonmail.com>
2021-11-13 10:37:34 +0200

    task 0.2

--- challenges/src/Main.java
+++ challenges/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthStyle;
+
 public class Main {
     public static void task1(){
         int x = 0;
@@ -9,11 +11,24 @@ public class Main {
         System.out.println(x);
         System.out.println(y);
     }
+    public static void task2(){
+        int x = 1 + 1 *2;
+        int y = (1 + 1) * 2;
+        int z = 1 + (1 * 2);
+        double a = 1 + 1 * 2 / 2.0;
+        double b = 3 /2.0;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+    }
 
 
 
     public static void main(String[] args) {
-	// write your code here
         task1();
+        task2();
     }
 }
